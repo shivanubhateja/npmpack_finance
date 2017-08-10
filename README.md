8b40a519f32fec14b183c5feeef7bb500cba2e99
shivanubhateja <shivanubhateja31@gmail.com>
2017-08-10 19:19:52 +0530

    pack

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# npmpack_finance
+# stock-directory
+
+Installation
+
+npm install stock-directory
+
+functions available
+
+getDetailsIntradayTimeSeries(stocksymbol: String, intervalInMinutes: Integer)
+
+ returns intraday time series (timestamp, open, high, low, close, volume) of the equity specified, updated realtime.s
---
+++ index.js
@@ -0,0 +1,13 @@
+var Promise = require("promise");
+var request = require("request");
+exports.getDetailsIntradayTimeSeries = function (symbol, interval) {
+    return new Promise(function (resolve, reject) {
+        request('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + symbol + '&interval=' + interval + 'min&apikey=W77LD4FPC2I3Z1X7', function (error, response, body) {
+            if (error) {
+                reject({ error: error, statusCode: response.statusCode });
+            } else {
+                resolve({ body: body, statusCode: response.statusCode });
+            }
+        })
+    });
+};
